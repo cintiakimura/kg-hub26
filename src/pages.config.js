b5f306a39332bd318881a3eb232069175441c7ae
Cintia Kimura <cintiakimura20@gmail.com>
2026-01-25 17:36:26 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,43 +1,4 @@
-import ClientLogin from './pages/ClientLogin';
-import SupplierLogin from './pages/SupplierLogin';
-import ManagerLogin from './pages/ManagerLogin';
-import ClientDashboard from './pages/ClientDashboard';
-import ClientVehicleAdd from './pages/ClientVehicleAdd';
-import ClientVehicleDetail from './pages/ClientVehicleDetail';
-import ClientQuotes from './pages/ClientQuotes';
-import ClientShipments from './pages/ClientShipments';
-import SupplierDashboard from './pages/SupplierDashboard';
-import ManagerDashboard from './pages/ManagerDashboard';
-import ManagerClients from './pages/ManagerClients';
-import ManagerSalesQuotes from './pages/ManagerSalesQuotes';
-import ManagerSupplierQuotes from './pages/ManagerSupplierQuotes';
-import ManagerLogistics from './pages/ManagerLogistics';
-import ManagerPurchases from './pages/ManagerPurchases';
-import ManagerFinancials from './pages/ManagerFinancials';
-import Home from './pages/Home';
-
-
-export const PAGES = {
-    "ClientLogin": ClientLogin,
-    "SupplierLogin": SupplierLogin,
-    "ManagerLogin": ManagerLogin,
-    "ClientDashboard": ClientDashboard,
-    "ClientVehicleAdd": ClientVehicleAdd,
-    "ClientVehicleDetail": ClientVehicleDetail,
-    "ClientQuotes": ClientQuotes,
-    "ClientShipments": ClientShipments,
-    "SupplierDashboard": SupplierDashboard,
-    "ManagerDashboard": ManagerDashboard,
-    "ManagerClients": ManagerClients,
-    "ManagerSalesQuotes": ManagerSalesQuotes,
-    "ManagerSupplierQuotes": ManagerSupplierQuotes,
-    "ManagerLogistics": ManagerLogistics,
-    "ManagerPurchases": ManagerPurchases,
-    "ManagerFinancials": ManagerFinancials,
-    "Home": Home,
-}
-
 export const pagesConfig = {
-    mainPage: "ClientLogin",
-    Pages: PAGES,
-};
+	mainPage: "Home",
+	Pages: {}
+}
